6d2f3b823e09405982707c5efe288e42d85e86bf
KyawZinThant22 <kyawthantver2@gmail.com>
2022-12-29 22:10:33 +0630

    CONST

---
+++ Const.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+    const double PI = 3.14;
+    double radius = 10;
+    double result = 2 * PI * radius;
+
+    std::cout << result << "\n";
+    return 0;
+}
