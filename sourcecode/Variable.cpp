1392ca4719edadbb91bf8389d712f4fffe3a043e
KyawZinThant22 <kyawthantver2@gmail.com>
2022-12-29 22:00:17 +0630

    remove exe

---
+++ sourcecode/Variable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    int x; //declartion
+    x= 5; // assigment
+
+    int y = 5;
+    int z = 5;
+    int sum = z +y ;
+
+    std::cout << sum << '\n';
+    return 0; 
+}
