59db25ab829eaae9f6aa56f07886e58ca2e26c33
KyawZinThant22 <kyawthantver2@gmail.com>
2022-12-29 22:04:32 +0630

    variable

--- Variable.cpp
+++ Variable.cpp
@@ -2,13 +2,35 @@
 
 int main() {
 
-    int x; //declartion
-    x= 5; // assigment
+    //int (whole number)
+    int age = 18;
+    int year = 2023;
+    int days = 7;
 
-    int y = 5;
-    int z = 5;
-    int sum = z +y ;
+    //double (number including decimal)
+    double price = 10.99;
+    double gpa = 2.5;
+    double temperature = 25.1;
 
-    std::cout << sum << '\n';
-    return 0; 
+    //char (single character)
+    char grade = 'A';
+    char initial = 'C';
+    char currency = '$';
+
+    //boolean (true or false)
+    bool student = false;
+    bool power = true;
+    bool forSale = true;
+
+    //string (objects that represent a sequence of text)
+    // std::string name = "Bro";
+    std::string name = "Kyaw Zin Thant";
+    std::string day = "Friday";
+    std::string food = "pizza";
+    std::string address = "123 Fake St.";
+
+    std::cout << "Hello " << name << '\n';
+    std::cout << "You are " << age << " years old";
+
+    return 0;
 }
